95174cdd2d438d11ec9ce82ccc3a742bc3cc102d
lijin <lijin@163.com>
2020-10-21 08:59:49 +0800

    谷粒商城第六天

---
+++ gmall-wms-interface/src/main/java/com/atguigu/gmall/wms/api/GmallWmsApi.java
@@ -0,0 +1,14 @@
+package com.atguigu.gmall.wms.api;
+
+import com.atguigu.gmall.common.bean.ResponseVo;
+import com.atguigu.gmall.wms.entity.WmsWareSkuEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import java.util.List;
+
+public interface GmallWmsApi {
+
+    @GetMapping("wms/waresku/sku/{skuId}")
+    public ResponseVo<List<WmsWareSkuEntity>> queryWareSkusBySkuId(@PathVariable("skuId")Long skuId);
+}
