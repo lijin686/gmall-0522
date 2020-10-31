6a519de826eb592941870077bce86509399f2ba1
lijin <lijin@163.com>
2020-11-01 07:56:29 +0800

    谷粒商城第十二天

--- gmall-index/src/main/java/com/atguigu/gmall/index/controller/IndexController.java
+++ gmall-index/src/main/java/com/atguigu/gmall/index/controller/IndexController.java
@@ -30,7 +30,7 @@ public class IndexController {
     @GetMapping("index/cates/{pid}")
     @ResponseBody
     public ResponseVo<List<CategoryEntity>>queryCategoryLvl2WithSubsById(@PathVariable("pid")Long pid){
-        List<CategoryEntity> categoryEntities = this.indexService.queryCategoryLvl2WithSubsById(pid);
+        List<CategoryEntity> categoryEntities = this.indexService.queryCategoryLvl2WithSubsById2(pid);
         return ResponseVo.ok(categoryEntities);
     }
 
@@ -40,8 +40,24 @@ public class IndexController {
     @GetMapping("index/test/lock")
     @ResponseBody
     public synchronized ResponseVo<Object> testLock() throws InterruptedException {
-        this.indexService.testLock();
+        this.indexService.testLock2();
         return ResponseVo.ok(null);
+    }
+
+    //测试闭锁
+    @GetMapping("index/test/latch")
+    @ResponseBody
+    public ResponseVo<Object> testLatch() throws InterruptedException {
+        String msg = this.indexService.testLatch();
+        return ResponseVo.ok(msg);
+    }
+
+    //测试闭锁
+    @GetMapping("index/test/down")
+    @ResponseBody
+    public ResponseVo<Object> testDown() throws InterruptedException {
+        String msg = this.indexService.testDown();
+        return ResponseVo.ok(msg);
     }
 
 
--- gmall-index/src/main/java/com/atguigu/gmall/index/service/IndexService.java
+++ gmall-index/src/main/java/com/atguigu/gmall/index/service/IndexService.java
@@ -2,10 +2,15 @@ package com.atguigu.gmall.index.service;
 
 import com.alibaba.fastjson.JSON;
 import com.atguigu.gmall.common.bean.ResponseVo;
+import com.atguigu.gmall.index.config.aspect.GmallCache;
 import com.atguigu.gmall.index.feign.GmallPmsClient;
+import com.atguigu.gmall.index.utils.DistributedLock;
 import com.atguigu.gmall.pms.api.GmallPmsApi;
 import com.atguigu.gmall.pms.entity.CategoryEntity;
 import org.apache.commons.lang.StringUtils;
+import org.redisson.api.RCountDownLatch;
+import org.redisson.api.RLock;
+import org.redisson.api.RedissonClient;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.stereotype.Service;
@@ -21,6 +26,12 @@ public class IndexService {
 
     @Autowired
     private StringRedisTemplate redisTemplate;
+
+    @Autowired
+    private DistributedLock distributedLock;
+
+    @Autowired
+    private RedissonClient redissonClient;
 
     private static final String KEY_PREFIX = "index:cate:";
 
@@ -41,24 +52,48 @@ public class IndexService {
         if(StringUtils.isNotBlank(json)){
             return JSON.parseArray(json,CategoryEntity.class);
         }
-        //如果没有命中缓存
+        //加分布式锁
+        RLock lock = this.redissonClient.getLock("lock" + pid);
+        lock.lock();
 
-        ResponseVo<List<CategoryEntity>> queryCategoryLvl2WithSubsById = this.pmsClient.queryCategoryLvl2WithSubsById(pid);
-        List<CategoryEntity> categoryEntities = queryCategoryLvl2WithSubsById.getData();
+        List<CategoryEntity> categoryEntities;
+        try {
+            String json2 = this.redisTemplate.opsForValue().get(KEY_PREFIX+pid);
+            if(StringUtils.isNotBlank(json2)){
+                return JSON.parseArray(json2,CategoryEntity.class);
+            }
+            //如果没有命中缓存
+
+            ResponseVo<List<CategoryEntity>> queryCategoryLvl2WithSubsById = this.pmsClient.queryCategoryLvl2WithSubsById(pid);
+            categoryEntities = queryCategoryLvl2WithSubsById.getData();
         /*
             解决缓存穿透问题，当大量请求访问缓存中不存在的数据，导致mysql宕机
             解决办法，就算缓存中不存在也要进行缓存 null ,但是缓存的时间不能过长
          */
-        if(CollectionUtils.isEmpty(categoryEntities)){
-            this.redisTemplate.opsForValue().set(KEY_PREFIX+pid,JSON.toJSONString(categoryEntities),5, TimeUnit.MINUTES);
-        }else{
-            /*
-                解决缓存雪崩问题，大量缓存数据同时过期，同时又大量请求访问过期数据
-                解决办法，给缓存时间添加随机值
-             */
-            this.redisTemplate.opsForValue().set(KEY_PREFIX+pid,JSON.toJSONString(categoryEntities),90+new Random().nextInt(5), TimeUnit.DAYS);
+            if(CollectionUtils.isEmpty(categoryEntities)){
+                this.redisTemplate.opsForValue().set(KEY_PREFIX+pid,JSON.toJSONString(categoryEntities),5, TimeUnit.MINUTES);
+            }else{
+                /*
+                    解决缓存雪崩问题，大量缓存数据同时过期，同时又大量请求访问过期数据
+                    解决办法，给缓存时间添加随机值
+                 */
+                this.redisTemplate.opsForValue().set(KEY_PREFIX+pid,JSON.toJSONString(categoryEntities),90+new Random().nextInt(5), TimeUnit.DAYS);
+
+            }
+            return categoryEntities;
+        } finally {
+            lock.unlock();
         }
+
+    }
+
+    //查询二级分类和三级分类(不用上面那种方法，使用自定义注解)
+    @GmallCache(prefix = KEY_PREFIX, lock = "lock:", timeout = 129600, random = 7200)
+    public List<CategoryEntity> queryCategoryLvl2WithSubsById2(Long pid) {
+        ResponseVo<List<CategoryEntity>> listResponseVo = this.pmsClient.queryCategoryLvl2WithSubsById(pid);
+        List<CategoryEntity> categoryEntities = listResponseVo.getData();
         return categoryEntities;
+
     }
 
     //测试分布式锁
@@ -94,6 +129,64 @@ public class IndexService {
                 this.redisTemplate.delete("lock");
             }
         }
+    }
+
+    //测试分布式锁，使用lua脚本
+    public void testLock1() throws InterruptedException {
+        String uuid = UUID.randomUUID().toString();
+        Boolean lock = this.distributedLock.tryLock("lock",uuid,9l);
+        if (lock) {
+            // 获取锁成功，执行业务逻辑
+            String countString = this.redisTemplate.opsForValue().get("count");
+            if (StringUtils.isBlank(countString)) {
+                this.redisTemplate.opsForValue().set("count", "1");
+            }
+            int count = Integer.parseInt(countString);
+            this.redisTemplate.opsForValue().set("count", String.valueOf(++count));
+
+            TimeUnit.SECONDS.sleep(60);
+
+            //this.testSubLock(uuid);
+
+            this.distributedLock.unlock("lock", uuid);
+        }
+    }
+
+    //测试分布式锁，使用 redisson
+    public void testLock2() throws InterruptedException {
+        String uuid = UUID.randomUUID().toString();
+        RLock lock = this.redissonClient.getLock("lock");
+        lock.lock();
+        // 获取锁成功，执行业务逻辑
+        String countString = this.redisTemplate.opsForValue().get("count");
+        if (StringUtils.isBlank(countString)) {
+            this.redisTemplate.opsForValue().set("count", "1");
+        }
+        int count = Integer.parseInt(countString);
+        this.redisTemplate.opsForValue().set("count", String.valueOf(++count));
+
+        //TimeUnit.SECONDS.sleep(60);
+        lock.unlock();
+
+    }
+
+    //测试闭锁
+    public String testLatch() throws InterruptedException {
+
+        RCountDownLatch latch = this.redissonClient.getCountDownLatch("countdowm");
+        latch.trySetCount(6);
+        latch.await();
+
+        return "班长锁门";
+
+    }
+
+    public String testDown() {
+        RCountDownLatch latch = this.redissonClient.getCountDownLatch("countdowm");
+        latch.countDown();
+        System.out.println(latch.getCount());
+        return "出来一个人";
+
     }
 }
 
